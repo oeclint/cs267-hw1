93c1f5a0bd5d9db8e848b84860c229d6d7c19813
Omar Clinton <oeclint@cori06.nersc.gov>
2019-02-06 22:04:13 -0800

    implemented unroll transpose

--- cs267_hw1_2019/dgemm-blocked.c
+++ cs267_hw1_2019/dgemm-blocked.c
@@ -1,6 +1,6 @@
 const char *dgemm_desc = "Simple blocked dgemm.";
 
-#define BLOCK_SIZE 500
+#define BLOCK_SIZE 120
 
 #if !defined(BLOCK_SIZE)
 #define BLOCK_SIZE 41
@@ -31,8 +31,122 @@ static void do_block(int lda, int M, int N, int K, double *A, double *B, double 
     }
 }
 
+static void do_block_unroll(int lda, int M, int N, int K, double *A, double *B, double *C) {
+    // For each row i of A
+    for (int i = 0; i < M; i+=2) {
+        //For each column j of B
+        for (int j = 0; j < N; j+=2) {
+            // Compute C(i,j)
+            double cij = C[i + j * lda];
+            // Compute C(i+1,j)
+            double ci1j = C[i + 1 + j * lda];
+            // Compute C(i,j+1)
+            double cij1 = C[i + (j + 1) * lda];
+            // Compute C(i+1,j+1)
+            double ci1j1 = C[(i + 1) + (j + 1) * lda];
+            for (int k = 0; k < K; ++k) {
+                cij += A[i + k * lda] * B[k + j * lda];
+                ci1j += A[i + 1 + k * lda] * B[k + j * lda];
+                cij1 += A[i + k * lda] * B[k + (j + 1) * lda];
+                ci1j1 += A[i + 1 + k * lda] * B[k + (j + 1) * lda];
+            }
+            C[i + j * lda] = cij;
+            C[i + 1 + j * lda] = ci1j;
+            C[i + (j + 1) * lda] = cij1;
+            C[(i + 1) + (j + 1) * lda] = ci1j1;
+        }
+    }
+}
+static void do_block_unroll_transpose(int lda, int M, int N, int K, double *A, double *B, double *C) {
+    // For each row i of A
+    for (int i = 0; i < M; i+=2) {
+        //For each column j of B
+        for (int j = 0; j < N; j+=2) {
+            // Compute C(i,j)
+            double cij = C[i + j * lda];
+            // Compute C(i+1,j)
+            double ci1j = C[i + 1 + j * lda];
+            // Compute C(i,j+1)
+            double cij1 = C[i + (j + 1) * lda];
+            // Compute C(i+1,j+1)
+            double ci1j1 = C[(i + 1) + (j + 1) * lda];
+            for (int k = 0; k < K; ++k) {
+                cij += A[k + i * lda] * B[k + j * lda];
+                ci1j += A[k + (i + 1)* lda] * B[k + j * lda];
+                cij1 += A[k + i * lda] * B[k + (j + 1) * lda];
+                ci1j1 += A[k + (i + 1) * lda] * B[k + (j + 1) * lda];
+            }
+            C[i + j * lda] = cij;
+            C[i + 1 + j * lda] = ci1j;
+            C[i + (j + 1) * lda] = cij1;
+            C[(i + 1) + (j + 1) * lda] = ci1j1;
+        }
+    }
+}
 
-static void do_block_vectorized(int lda, int M, int N, int K, double *A, double *B, double *C) {
+static void do_block_unroll_transpose_vect1(int lda, int M, int N, int K, double *A, double *B, double *C) {
+    
+    __m256d vectorA1;
+    __m256d vectorB1;
+    __m256d vectorA2;
+    __m256d vectorB2;
+    __m256d vectorCij;
+    __m256d vectorCi1j;
+    __m256d vectorCij1;
+    __m256d vectorCi1j1;
+    // For each row i of A
+    for (int i = 0; i < M; i+=2) {
+        //For each column j of B
+        for (int j = 0; j < N; j+=2) {
+            // Compute C(i,j)
+            double cij[4] = {C[i + j * lda], 0, 0, 0};
+            vectorCij = _mm256_loadu_pd(cij);
+            // Compute C(i+1,j)
+            double ci1j[4] = {C[i + 1 + j * lda], 0, 0, 0};
+            vectorCi1j = _mm256_loadu_pd(ci1j);
+            // Compute C(i,j+1)
+            double cij1[4] = {C[i + (j + 1) * lda], 0, 0, 0};
+            vectorCij1 = _mm256_loadu_pd(cij1);
+            // Compute C(i+1,j+1)
+            double ci1j1[4] = {C[(i + 1) + (j + 1) * lda], 0, 0, 0};
+            vectorCi1j1 = _mm256_loadu_pd(ci1j1);
+            for (int k = 0;  k < K / 4 * 4; k+=4) {
+                vectorA1 = _mm256_loadu_pd(A + (k+i*lda));
+                vectorB1 = _mm256_loadu_pd(B + (k+j*lda));
+                vectorA2 = _mm256_loadu_pd(A + (k+(i+1)*lda));
+                vectorB2 = _mm256_loadu_pd(B + (k+(j+1)*lda));
+                vectorCij = _mm256_add_pd(vectorCij, _mm256_mul_pd(vectorA1, vectorB1));
+                vectorCi1j = _mm256_add_pd(vectorCi1j, _mm256_mul_pd(vectorA2, vectorB1));
+                vectorCij1 = _mm256_add_pd(vectorCij1, _mm256_mul_pd(vectorA1, vectorB2));
+                vectorCi1j1 = _mm256_add_pd(vectorCi1j1, _mm256_mul_pd(vectorA2, vectorB2));
+                //cij += A[k + i * lda] * B[k + j * lda];
+                //ci1j += A[k + (i + 1)* lda] * B[k + j * lda];
+                //cij1 += A[k + i * lda] * B[k + (j + 1) * lda];
+                //ci1j1 += A[k + (i + 1) * lda] * B[k + (j + 1) * lda];
+            }
+
+            _mm256_store_pd(cij, vectorCij);
+            _mm256_store_pd(ci1j, vectorCi1j);
+            _mm256_store_pd(cij1, vectorCij1);
+            _mm256_store_pd(ci1j1, vectorCi1j1);
+
+            for (int k = K / 4 * 4; k < K; k++) {
+                cij[0] += A[k + i * lda] * B[k + j * lda];
+                ci1j[0] += A[k + (i + 1)* lda] * B[k + j * lda];
+                cij1[0] += A[k + i * lda] * B[k + (j + 1) * lda];
+                ci1j1[0] += A[k + (i + 1) * lda] * B[k + (j + 1) * lda];
+            }
+
+            C[i + j * lda] = cij[0] + cij[1] + cij[2] + cij[3];
+            C[i + 1 + j * lda] = ci1j[0] + ci1j[1] + ci1j[2] + ci1j[3];
+            C[i + (j + 1) * lda] = cij1[0] + cij1[1] + cij1[2] + cij1[3];
+            C[(i + 1) + (j + 1) * lda] = ci1j1[0] + ci1j1[1] + ci1j1[2] + ci1j1[3];
+        }
+    }
+}
+
+
+static void do_block_transpose_vect(int lda, int M, int N, int K, double *A, double *B, double *C) {
 
     __m256d vectorA;
     __m256d vectorB;
@@ -69,7 +183,7 @@ static void do_block_vectorized(int lda, int M, int N, int K, double *A, double 
     }
 }
 
-static void do_block_vectorized_unroll4(int lda, int M, int N, int K, double *A, double *B, double *C) {
+static void do_block_unroll_transpose_vect2(int lda, int M, int N, int K, double *A, double *B, double *C) {
 
     __m256d vectorA;
     __m256d vectorB;
@@ -158,8 +272,7 @@ void square_dgemm(int lda, double *A, double *B, double *C) {
                 int N = min (BLOCK_SIZE, lda - j);
                 int K = min (BLOCK_SIZE, lda - k);
                 // Perform individual block dgemm
-                do_block_vectorized_unroll4(lda, M, N, K, A_t + k + i * lda, B + k + j * lda, C + i + j * lda);
-
+                do_block_unroll_transpose(lda, M, N, K, A_t + k + i * lda, B + k + j * lda, C + i + j * lda);
                 //do_block(lda, M, N, K, A + i + k*lda, B + k + j*lda, C + i + j*lda);
             }
         }

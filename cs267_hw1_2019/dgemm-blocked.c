11f1d206d828ebd04cc9831a86edb218b6e01d89
Alex Thomas <alexthomas@berkeley.edu>
2019-02-10 02:01:32 +0000

    loop unroll 2x

--- cs267_hw1_2019/dgemm-blocked.c
+++ cs267_hw1_2019/dgemm-blocked.c
@@ -105,7 +105,7 @@ static void do_block_unroll_transpose_fix(int lda, int M, int N, int K, double *
 
 //    double ci3j, ci3j1, ci3j2, ci3j3, ci2j3, ci1j3, cij3;
 
-    double t0, t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13, t14, t15, t16, t17, t18, t19, t20, t21, t22, t23;
+    double t0, t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11;// , t12, t13, t14, t15, t16, t17, t18, t19, t20, t21, t22, t23;
     int i, j, k;
 
     for (i = 0; i < M / 3 * 3; i += 3) {
@@ -149,7 +149,7 @@ static void do_block_unroll_transpose_fix(int lda, int M, int N, int K, double *
 //            cij3 = C[(i) + (j + 3) * lda];
 
 
-            for (k = 0; k < K/4 * 4; k += 4) {
+            for (k = 0; k < K/2 * 2; k += 2) {
                 //1st Row
                 t0 = A[k + i * lda];
                 //1st Col
@@ -178,31 +178,31 @@ static void do_block_unroll_transpose_fix(int lda, int M, int N, int K, double *
                 t11 = B[k + 1 + (j + 2) * lda];
 
 
-                //1st Row
-                t12 = A[k + 2 + i * lda];
-                //1st Col
-                t13 = B[k + 2 + j * lda];
-                //2nd Row
-                t14 = A[k + 2 + (i + 1) * lda];
-                //2nd Col
-                t15 = B[k + 2 + (j + 1) * lda];
-                //3rd Row
-                t16 = A[k + 2 + (i + 2) * lda];
-                //3rd Col
-                t17 = B[k + 2 + (j + 2) * lda];
-
-                //1st Row
-                t18 = A[k + 3 + i * lda];
-                //1st Col
-                t19 = B[k + 3 + j * lda];
-                //2nd Row
-                t20 = A[k + 3 + (i + 1) * lda];
-                //2nd Col
-                t21 = B[k + 3 + (j + 1) * lda];
-                //3rd Row
-                t22 = A[k + 3 + (i + 2) * lda];
-                //3rd Col
-                t23 = B[k + 3 + (j + 2) * lda];
+//                //1st Row
+//                t12 = A[k + 2 + i * lda];
+//                //1st Col
+//                t13 = B[k + 2 + j * lda];
+//                //2nd Row
+//                t14 = A[k + 2 + (i + 1) * lda];
+//                //2nd Col
+//                t15 = B[k + 2 + (j + 1) * lda];
+//                //3rd Row
+//                t16 = A[k + 2 + (i + 2) * lda];
+//                //3rd Col
+//                t17 = B[k + 2 + (j + 2) * lda];
+//
+//                //1st Row
+//                t18 = A[k + 3 + i * lda];
+//                //1st Col
+//                t19 = B[k + 3 + j * lda];
+//                //2nd Row
+//                t20 = A[k + 3 + (i + 1) * lda];
+//                //2nd Col
+//                t21 = B[k + 3 + (j + 1) * lda];
+//                //3rd Row
+//                t22 = A[k + 3 + (i + 2) * lda];
+//                //3rd Col
+//                t23 = B[k + 3 + (j + 2) * lda];
 
 
 
@@ -215,16 +215,16 @@ static void do_block_unroll_transpose_fix(int lda, int M, int N, int K, double *
 //                t7 = B[k + (j + 3) * lda];
 
 
-                cij += t0 * t1 + t6*t7 + t12 * t13 + t18*t19;
-                ci1j += t2 * t1 + t8*t7 + t13 * t14 + t19*t20;
-                cij1 += t0 * t3 + t6 * t9 + t12*t15 + t18*t21;
-                ci1j1 += t2 * t3 + t8*t9 + t14*t15 + t20*t21;
+                cij += t0 * t1 + t6*t7; //+ t12 * t13 + t18*t19;
+                ci1j += t2 * t1 + t8*t7; // + t13 * t14 + t19*t20;
+                cij1 += t0 * t3 + t6 * t9; // + t12*t15 + t18*t21;
+                ci1j1 += t2 * t3 + t8*t9; // + t14*t15 + t20*t21;
 
-                ci2j += t4 * t1 + t10*t7 + t16*t13 + t22*t19;
-                ci2j1 += t4 * t3 + t10*t9 + t16*t15 + t22*t21;
-                ci2j2 += t5 * t4 + t11*t10 + t17*t16 + t23*t22;
-                ci1j2 += t2 * t5 + t8*t11 + t14*t17 + t20*t23;
-                cij2 += t0 * t5 + t6*t11 + t12*t17 + t18*t23;
+                ci2j += t4 * t1 + t10*t7; // + t16*t13 + t22*t19;
+                ci2j1 += t4 * t3 + t10*t9; // + t16*t15 + t22*t21;
+                ci2j2 += t5 * t4 + t11*t10; // + t17*t16 + t23*t22;
+                ci1j2 += t2 * t5 + t8*t11; // + t14*t17 + t20*t23;
+                cij2 += t0 * t5 + t6*t11; // + t12*t17 + t18*t23;
 
 //                ci3j += t6 * t1;
 //                ci3j1 += t6 * t3;
@@ -238,7 +238,7 @@ static void do_block_unroll_transpose_fix(int lda, int M, int N, int K, double *
             }
 
 
-            for(int k = K/4*4; k < K; k++){
+            for(int k = K/2*2; k < K; k++){
                 //1st Row
                 t0 = A[k + i * lda];
                 //1st Col
